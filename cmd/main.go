499ace6a2270e2c6a61a794d713cc3905bd7f9f0
Will Bollock <wbollock@linode.com>
2025-01-02 13:42:25 -0500

    feat: read config and CLI flags
    initial CLI flags for config path and read canary config. also includes
    sample/test config

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,51 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
+	"o11y-canary/internal/config"
 	"os"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
-	slog.SetDefault(logger)
+
+	defaultLogLevel := "info"
+
+	logLevel := flag.String("log.level", defaultLogLevel, "Set log level (options: info, warn, error, debug)")
+	configFileFlag := flag.String("config", "test/config.yaml", "Path to the configuration file")
+	flag.Parse()
+
+	var slogLevel slog.Level
+	switch *logLevel {
+	case "debug":
+		slogLevel = slog.LevelDebug
+	case "info":
+		slogLevel = slog.LevelInfo
+	case "warn":
+		slogLevel = slog.LevelWarn
+	case "error":
+		slogLevel = slog.LevelError
+	default:
+		slogLevel = slog.LevelInfo
+	}
+
+	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slogLevel})
+
+	slog.SetDefault(slog.New(handler))
+	slog.Info("Logger initialized", "level", *logLevel)
+
+	file, err := os.Open(*configFileFlag)
+	if err != nil {
+		slog.Error("Error decoding YAML", "error", err)
+	}
+	defer file.Close()
+
+	var config config.CanaryConfig
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		slog.Error("Error decoding YAML", "error", err)
+	}
 
 }
